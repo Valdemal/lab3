ffea57d7013830114e92b740372a20819332d4ac
Vladimir Nekhaenko <59586081+Valdemal@users.noreply.github.com>
2021-03-23 17:29:55 +0300

    Pre-last version

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <windows.h>
 
 
-int32_t get_bin_size(long n) {
-    // Возвращает количество значащих бит в двоичной записи числа n
-    int32_t i = 0;
+// Возвращает количество цифр в двоичной записи числа n
+u_long get_bin_size(u_long n) {
+
+    u_long i = 0;
     while (n) {
         n >>= 1;
         ++i;
@@ -13,55 +13,61 @@ int32_t get_bin_size(long n) {
     return i;
 }
 
-void binary_output(long n) {
-    // Выводит двоичную запись числа n
-    if (n != 0) {
-        long mask = 1 << (get_bin_size(n) - 1);
-        for (; mask; mask >>= 1) {
-            putchar(mask & n ? '1' : '0');
-        }
-    } else putchar('0');
-}
-
-long create_mask(int32_t n) {
-    /* возвращает маску, содержащую n едениц в младщих разрядах */
+/* возвращает маску, содержащую n едениц в младших разрядах */
+u_long create_mask(u_long n) {
     return ~(~0 << n);
 }
 
-long replace_occurrences(long n, long entry) {
-    /* Возвращает число, которое является результатом замены нулями
-     * вхождения двоичной записи entry в двоичную запись n*/
-    if (n == 0) return 0;
+// Выводит двоичную запись числа n
+void out_bin(u_long n) {
+    if (n != 0) {
+        u_long mask = 1 << (sizeof(u_long) * 8 - 1);
+        while ((mask & n) == 0)
+            mask >>= 1;
 
-    int32_t entry_bin_size = get_bin_size(entry);
-    long mask = create_mask(entry_bin_size), n_copy = n;
+        while (mask) {
+            putchar(mask & n ? '1' : '0');
+            mask >>= 1;
+        }
+    } else
+        putchar('0');
+}
 
-    size_t size = 1 + (get_bin_size(n) - get_bin_size(entry));
+/* Замена в вхождения entry в двоичной форме числа n нулями*/
+u_long get_replaced(u_long n, u_long entry) {
+    if (n == 0)
+        return 0;
 
-    for (size_t i = 1; i <= size; i++) {
-        if ((n & mask) == entry)
-            n_copy &= ~mask;
-        mask <<= 1;
-        entry <<= 1;
+    u_long entry_bin_size = get_bin_size(entry);
+    u_long mask = create_mask(entry_bin_size);
+
+    while (n >= entry) {
+        if ((n & mask) == entry) {
+            n &= ~mask;
+            mask <<= entry_bin_size;
+            entry <<= entry_bin_size;
+        } else {
+            mask <<= 1;
+            entry <<= 1;
+        }
     }
 
-    return n_copy;
+    return n;
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);//Подключение русского языка
 
-    long n;
+    u_long n;
     printf("Введите длинное целое число:");
     scanf("%ld", &n);
     printf("Ваше число: %ld в 2 СС : ", n);
-    binary_output(n);
-
-    long result = replace_occurrences(n, 6);
-
+    out_bin(n);
+    printf("\n%d", get_bin_size(-1));
+    u_long result = get_replaced(n, 6);
     printf("\n");
     printf("Ваше число в 2 СС, в котором вхождения 110 заменены на 000: ");
-    binary_output(result);
+    out_bin(result);
     printf("\n");
     printf("Ваше число в 10 СС в котором вхождения 110 заменены на 000: %ld\n",
            result);
